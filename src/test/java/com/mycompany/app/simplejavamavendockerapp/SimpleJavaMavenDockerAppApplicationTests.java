46fdb3bc5f7c1b1db0a6afdf6ed9f504c1b9b5ac
hcd0097 <lang.he@baozun.com>
2025-12-01 16:10:05 +0800

    init

---
+++ src/test/java/com/mycompany/app/simplejavamavendockerapp/SimpleJavaMavenDockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.simplejavamavendockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaMavenDockerAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
